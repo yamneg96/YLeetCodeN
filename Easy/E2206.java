6963a996b7cc5612547886dfda9ae708c724c570
Yamlak Negash <yamlaknegash96@gmail.com>
2025-03-17 20:47:57 +0300

    Added 2nd file for Easy

---
+++ Easy/E2206.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class E2206{
+  public static boolean divideArray(int[] nums) {
+    int len = nums.length;
+    int n = len / 2;
+    int newarr[] = new int[len];
+
+    if(len % 2 != 0){
+      return false;
+    }
+    else{
+      for (int i=0; i<len; i++){
+        if((nums[i] < nums[i++]) && i++ <= len){
+          newarr[i] = nums[i];
+        }
+      }
+      return true;
+    }
+}
+
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter the length of the array: ");
+    int n = input.nextInt();
+    int[] nums = new int[n];
+    System.out.println("Enter the elements of the array: ");
+    for (int i = 0; i < n; i++) {
+      nums[i] = input.nextInt();
+    }
+    boolean result = divideArray(nums);
+    System.out.println(result);
+    input.close();
+  }
+}
