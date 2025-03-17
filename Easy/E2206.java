66a0506245fcb3a744b081bc6f4971acf0ebb6b0
Yamlak Negash <yamlaknegash96@gmail.com>
2025-03-17 21:30:12 +0300

    Added 2nd file for Easy

--- Easy/E2206.java
+++ Easy/E2206.java
@@ -1,44 +1,26 @@
-import java.util.Scanner;
+import java.util.HashMap;
 
-public class E2206{
-  public static boolean divideArray(int[] nums) {
-    int len = nums.length;
-    int n = len / 2;
-    int newarr[] = new int[len];
+public class E2206 {
+    public static boolean divideArray(int[] nums) {
+        HashMap<Integer, Integer> freq = new HashMap<>();
 
-    if(len % 2 != 0){
-      return false;
-    }
-    else{
-      for (int i=0; i<len; i++){
-        if((nums[i] < nums[i++]) && i++ <= len){
-          newarr[i] = nums[i];
-          //  System.out.println(newarr[i]);
-          //  System.out.println(nums[i]);
-          //  System.out.println(i);
-
+        // Count frequencies
+        for (int num : nums) {
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
         }
-      }
-      return true;
-    }
-}
 
-  public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
-    System.out.print("Enter the length of the array: ");
-    int n = input.nextInt();
-    int[] nums = new int[n];
-    System.out.println("Enter the elements of the array: ");
-    for (int i = 0; i < n; i++) {
-      nums[i] = input.nextInt();
+        // Check if all counts are even
+        for (int count : freq.values()) {
+            if (count % 2 != 0) {
+                return false;
+            }
+        }
+        return true;
     }
-    boolean result = divideArray(nums);
-    System.out.println(result);
-    input.close();
 
-    // int[] nums = {1, 2, 3, 4, 5, 6};
-    // boolean result = divideArray(nums);
-    // System.out.println(result);
-      
-  }
+    // Test Cases
+    public static void main(String[] args) {
+        System.out.println(divideArray(new int[]{3, 2, 3, 2, 2, 2})); // Output: true
+        System.out.println(divideArray(new int[]{1, 2, 3, 4}));       // Output: false
+    }
 }
